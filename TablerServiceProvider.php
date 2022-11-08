1bcff5ec7573f353a04ef31d9fa27660e81c3555
aerrata <0x0976@gmail.com>
2022-11-08 10:01:19 +0800

    init

---
+++ TablerServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tabler\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TablerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/src/config/tabler.php',
+            'tabler'
+        );
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/src/resources/views', 'tabler');
+
+        $this->publishes([
+            __DIR__ . '/src/config/tabler.php' => config_path('tabler.php'),
+        ]);
+
+        $this->publishes([
+            __DIR__ . '/src/public/vendor' => public_path('vendor'),
+        ]);
+    }
+}
