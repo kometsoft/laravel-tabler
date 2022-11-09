a19adbf5922387060e79cf11725d5da43712d887
aerrata <0x0976@gmail.com>
2022-11-09 17:05:39 +0800

    feat

---
+++ src/resources/js/tabler.js
@@ -0,0 +1,23 @@
+import noUiSlider from 'nouislider'
+window.noUiSlider = noUiSlider
+
+import Litepicker from 'litepicker'
+window.Litepicker = Litepicker
+
+import TomSelect from 'tom-select'
+window.TomSelect = TomSelect
+
+import axios from 'axios'
+window.axios = axios
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+// import $ from 'jquery'
+// window.$ = $
+
+// import Alpine from 'alpinejs'
+// window.Alpine = Alpine
+// Alpine.start()
+
+import '@tabler/core/src/js/tabler'
+import '@tabler/core/src/js/demo'
+import '@tabler/core/src/js/demo-theme'
